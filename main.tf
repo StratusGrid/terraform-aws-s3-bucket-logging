a68347a7c4c5aa1afae34618df441b993d005736
John Lewis <john.lewis@stratusgrid.com>
2021-10-06 22:43:01 -0400

    Enable public access block for logging bucket

--- main.tf
+++ main.tf
@@ -125,3 +125,12 @@ resource "aws_s3_bucket_policy" "bucket_policy_attachment" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.bucket_policy.json
 }
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
