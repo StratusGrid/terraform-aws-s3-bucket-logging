030ec958ce993aa27e4be9fcc5d56bcfe91a62f3
wesleykirklandsg <92735382+wesleykirklandsg@users.noreply.github.com>
2022-01-10 11:48:28 -0500

    Enable versioning support (#4)
    * Enable versioning support
    
    * Terradocs Support

---
+++ examples/regional.tf
@@ -0,0 +1,11 @@
+module "s3_bucket_logging_us_east_2" {
+  source      = "StratusGrid/s3-bucket-logging/aws"
+  version     = "1.2.2"
+  name_prefix = var.name_prefix
+  name_suffix = "${local.name_suffix}-us-east-2"
+  input_tags  = merge(local.common_tags, {})
+  providers = {
+    aws = aws.us-east-2
+  }
+  versioning_enabled = true #Enabled by default
+}
---
+++ examples/simple.tf
@@ -0,0 +1,7 @@
+module "s3_bucket_logging" {
+  source  = "StratusGrid/s3-bucket-logging/aws"
+  version = "1.2.2"
+  name_prefix = "${var.name_prefix}"
+  input_tags = local.common_tags
+  versioning_enabled = true #Enabled by default
+}
