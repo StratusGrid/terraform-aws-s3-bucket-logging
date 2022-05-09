e8e9437f2acf4bc0ea72dfc90b4595bae50d02d9
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-05-09 11:19:04 -0400

    Fixed Resource Error

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
+  bucket = aws_s3_bucket.bucket.bucket
 
   rule {
     apply_server_side_encryption_by_default {
